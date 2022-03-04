459696c93e746926c34977f3d3b4f4acffb3ec18
Mourathiago <moura.francisco.thiago@hotmail.com>
2022-03-03 22:14:17 -0400

    Insert Implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,12 +10,17 @@ public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("===== TEST 1: seller findById =====");
+        System.out.println("===== TEST 1: department findById =====");
         Department department = departmentDao.findById(1);
         System.out.println(department);
 
-        System.out.println("\n===== TEST 2: seller findAll =====");
+        System.out.println("\n===== TEST 2: department findAll =====");
         List<Department> departments = departmentDao.findAll();
         departments.forEach(System.out::println);
+
+        System.out.println("\n===== TEST 3: department insert =====");
+        department = new Department(null, "Teste");
+        departmentDao.insert(department);
+        System.out.println("Insert completed! Id = " + department.getId());
     }
 }
