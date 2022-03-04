e0976e953a5b10fd8f3e8eacf788e3e25b99e0a1
Mourathiago <moura.francisco.thiago@hotmail.com>
2022-03-03 22:47:22 -0400

    Delete Implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -8,6 +9,7 @@ import model.entities.Department;
 
 public class Program2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("===== TEST 1: department findById =====");
@@ -28,5 +30,13 @@ public class Program2 {
         department.setName("D1");
         departmentDao.update(department);
         System.out.println("Updated completed!");
+
+        System.out.println("\n===== TEST 5: department delete =====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Deleted completed!");
+
+        sc.close();
     }
 }
